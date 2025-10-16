7a91adf2399e143e1148675a55b3cf5d660c9e6d
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-16 15:03:31 +0200

    Update variables.tf

--- PracticaFinal/infra/terraform/variables.tf
+++ PracticaFinal/infra/terraform/variables.tf
@@ -22,3 +22,19 @@ variable "node_size" {
   type    = string
   default = "Standard_DS2_v2"
 }
+
+variable "azure_subscription_id" {
+  type = string
+}
+
+variable "azure_client_id" {
+  type = string
+}
+
+variable "azure_client_secret" {
+  type = string
+}
+
+variable "azure_tenant_id" {
+  type = string
+}
