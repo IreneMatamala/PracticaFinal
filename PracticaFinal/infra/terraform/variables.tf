d5e46a449d1e394ee0b0b6e7584b1cda2d5fbaaa
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-16 13:45:10 +0200

    Update variables.tf

--- PracticaFinal/infra/terraform/variables.tf
+++ PracticaFinal/infra/terraform/variables.tf
@@ -1,6 +1,24 @@
-variable "subscription_id" { type = string }
-variable "resource_group" { type = string, default = "rg-practicafinal" }
-variable "location" { type = string, default = "westeurope" }
-variable "aks_name" { type = string, default = "aks-practicafinal" }
-variable "node_count" { type = number, default = 2 }
-variable "node_size" { type = string, default = "Standard_DS2_v2" }
+variable "resource_group" {
+  type    = string
+  default = "rg-practicafinal"
+}
+
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "aks_name" {
+  type    = string
+  default = "aks-practicafinal"
+}
+
+variable "node_count" {
+  type    = number
+  default = 2
+}
+
+variable "node_size" {
+  type    = string
+  default = "Standard_DS2_v2"
+}
