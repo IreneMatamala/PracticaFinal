d627d55887a9ea8a721acda0dae67f11d1fa2db3
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-16 12:38:13 +0200

    Update Terraform configuration to version 1.3

--- PracticaFinal/infra/terraform/main.tf
+++ PracticaFinal/infra/terraform/main.tf
@@ -1,40 +1,14 @@
-cat > infra/terraform/main.tf << 'EOF'
 terraform {
-  required_version = ">=1.0"
+  required_version = ">= 1.3"
   required_providers {
-    azurerm = { source = "hashicorp/azurerm", version = "~>3.0" }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0"
+    }
   }
+  backend "local" {}
 }
 
-provider "azurerm" { features {} }
-
-resource "azurerm_resource_group" "PracticaFinal" {
-  name     = "rg-practicafinal-${var.environment}"
-  location = var.location
+provider "azurerm" {
+  features = {}
 }
-
-resource "azurerm_kubernetes_cluster" "PracticaFinal" {
-  name                = "aks-practicafinal-${var.environment}"
-  location            = azurerm_resource_group.PracticaFinal.location
-  resource_group_name = azurerm_resource_group.PracticaFinal.name
-  dns_prefix          = "practicafinal-${var.environment}"
-  kubernetes_version  = var.kubernetes_version
-
-  default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_B2s"
-  }
-
-  identity { type = "SystemAssigned" }
-  network_profile { network_plugin = "azure" }
-}
-
-resource "azurerm_container_registry" "PracticaFinal" {
-  name                = "acrpracticafinal${var.environment}"
-  resource_group_name = azurerm_resource_group.PracticaFinal.name
-  location            = azurerm_resource_group.PracticaFinal.location
-  sku                 = "Basic"
-  admin_enabled       = true
-}
-EOF
