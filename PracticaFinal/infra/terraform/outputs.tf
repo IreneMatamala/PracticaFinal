b9b3bf53828dff9f374032b31f9d04bbb644a988
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-16 16:19:17 +0200

    Update outputs.tf

--- PracticaFinal/infra/terraform/outputs.tf
+++ PracticaFinal/infra/terraform/outputs.tf
@@ -1,14 +1,21 @@
-output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks.name
-}
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_admin_config[0].raw_config
-  sensitive = true
+  description = "Kubernetes configuration"
+  value       = azurerm_kubernetes_cluster.aks.kube_admin_config[0]
+  sensitive   = true
 }
 
 
+output "host" {
+  description = "Kubernetes cluster host"
+  value       = azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
+  sensitive   = true
+}
 
 
+output "configure_kubectl" {
+  description = "Command to configure kubectl"
+  value       = "az aks get-credentials --resource-group ${azurerm_resource_group.main.name} --name ${azurerm_kubernetes_cluster.aks.name} --overwrite-existing"
+}
 
 
 
