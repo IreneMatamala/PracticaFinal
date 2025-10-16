45b79f5dffa4426427b3f4183a150c64bca571a4
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-16 15:07:13 +0200

    Update outputs.tf

--- PracticaFinal/infra/terraform/outputs.tf
+++ PracticaFinal/infra/terraform/outputs.tf
@@ -2,6 +2,7 @@ output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config.0.raw_kube_config
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
   sensitive = true
 }
+
