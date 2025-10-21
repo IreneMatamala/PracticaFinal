c939719ff3bd4aa27cb3c9f46c09a01e37e37a0e
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-21 15:09:01 +0200

    Update k8s-cluster.tf

--- PracticaFinal/infra/terraform/k8s-cluster.tf
+++ PracticaFinal/infra/terraform/k8s-cluster.tf
@@ -1,13 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "techwave-rg-nuevo"
-  location = "francecentral"
+  name     = "techwave-rg"
+  location = "westeurope"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "techwave-vnet"
-  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
@@ -17,19 +21,27 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_container_registry" "acr" {
+  name                     = "techwaveacr123"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                      = "Basic"
+  admin_enabled            = true
+  public_network_access_enabled = true
+}
+
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = "techwave-aks"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "techwave"
-  kubernetes_version  = "1.27.3"
 
   default_node_pool {
-    name         = "default"
-    node_count   = 1
-    vm_size      = "Standard_B2s"
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
     os_disk_type = "Managed"
-    type         = "VirtualMachineScaleSets"
+    type       = "VirtualMachineScaleSets"
   }
 
   identity {
@@ -40,18 +52,33 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     network_plugin    = "azure"
     load_balancer_sku = "standard"
   }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  depends_on = [azurerm_subnet.subnet]
 }
 
-output "aks_cluster_name" {
+resource "azurerm_role_assignment" "aks_acr" {
+  principal_id   = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope          = azurerm_container_registry.acr.id
+}
+
+output "aks_name" {
   value = azurerm_kubernetes_cluster.aks_cluster.name
 }
 
-output "aks_cluster_resource_group" {
+output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "aks_cluster_kubeconfig" {
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "kubeconfig" {
   value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
   sensitive = true
 }
-
