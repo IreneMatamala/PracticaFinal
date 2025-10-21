82e7f4e4b00b2fd09da6fac8c624b63bfd99e62f
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-21 15:35:23 +0200

    Update main.tf

--- PracticaFinal/infra/terraform/main.tf
+++ PracticaFinal/infra/terraform/main.tf
@@ -4,11 +4,21 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.77.0"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "techwave-rg"
+  location = "westeurope"
 }
