e6ad2bbd564611b6c480d5cd55fe3f04f1c2dd7b
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-21 13:26:52 +0200

    Update outputs.tf

--- PracticaFinal/infra/terraform/outputs.tf
+++ PracticaFinal/infra/terraform/outputs.tf
@@ -1,38 +1,14 @@
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
-  description = "Kubeconfig for the AKS cluster"
-  sensitive = true
-}
-
-
-
-output "cluster_name" {
-  description = "AKS cluster name"
-  value       = azurerm_kubernetes_cluster.aks.name
-}
-
-
 output "resource_group_name" {
-  description = "Resource group name"
-  value       = azurerm_resource_group.main.name
+  value = azurerm_resource_group.main.name
 }
 
-
-output "configure_kubectl" {
-  description = "Command to configure kubectl"
-  value       = "az aks get-credentials --resource-group ${azurerm_resource_group.main.name} --name ${var.aks_cluster_name} --overwrite-existing"
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
 }
 
-
-output "cluster_fqdn" {
-  description = "Cluster FQDN"
-  value       = azurerm_kubernetes_cluster.aks.fqdn
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
 }
 
-
-
-
-
-
-
-
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks
