4d06659ae7fe2967ee847298d12a973b6cd3dd95
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-21 16:23:53 +0200

    Update main.tf

--- PracticaFinal/infra/terraform/main.tf
+++ PracticaFinal/infra/terraform/main.tf
@@ -52,10 +52,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   }
 }
 
-resource "azurerm_role_assignment" "aks_acr" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
-}
+
+
 
 
