f095f10f43867d76885fdf69f78d2543057d1042
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-21 16:11:51 +0200

    Update outputs.tf

--- PracticaFinal/infra/terraform/outputs.tf
+++ PracticaFinal/infra/terraform/outputs.tf
@@ -3,8 +3,10 @@ output "aks_name" {
 }
 
 output "kubeconfig" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+  sensitive = true
 }
+
 
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
@@ -13,3 +15,4 @@ output "resource_group_name" {
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
+
