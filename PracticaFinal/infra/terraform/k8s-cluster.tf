27f661ed36afa8b16d9de139f657abd926c9b5be
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-21 14:56:06 +0200

    Update k8s-cluster.tf

--- PracticaFinal/infra/terraform/k8s-cluster.tf
+++ PracticaFinal/infra/terraform/k8s-cluster.tf
@@ -1,19 +1,22 @@
+
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.cluster_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  name                = "techwave-aks"
+  location            = "francecentral"
+  resource_group_name = azurerm_resource_group.main.name
   dns_prefix          = "techwave"
-  kubernetes_version  = "1.27.3"
+
+  kubernetes_version = "1.27.3"  # versión estable
 
   default_node_pool {
     name       = "default"
-    node_count = var.node_count
-    vm_size    = var.node_size
+    node_count = 1
+    vm_size    = "Standard_B2s"
   }
 
   identity {
     type = "SystemAssigned"
   }
+}
 
   role_based_access_control_enabled = true
 
