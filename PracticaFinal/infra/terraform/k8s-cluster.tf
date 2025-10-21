409e52802df304b74d0e0b9860193f349536dda0
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-21 14:57:53 +0200

    Update k8s-cluster.tf

--- PracticaFinal/infra/terraform/k8s-cluster.tf
+++ PracticaFinal/infra/terraform/k8s-cluster.tf
@@ -1,28 +1,81 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "techwave-rg"
+  location = "francecentral"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "techwave-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "techwave-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                     = "techwaveacr123"
+  location                 = azurerm_resource_group.main.location
+  resource_group_name      = azurerm_resource_group.main.name
+  sku                      = "Basic"
+  admin_enabled            = true
+  public_network_access_enabled = true
+}
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "techwave-aks"
-  location            = "francecentral"
+  location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   dns_prefix          = "techwave"
-
-  kubernetes_version = "1.27.3"  # versión estable
+  kubernetes_version  = "1.27.3"
 
   default_node_pool {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_B2s"
+    os_disk_type = "Managed"
+    type       = "VirtualMachineScaleSets"
   }
 
   identity {
     type = "SystemAssigned"
   }
-}
 
-role_based_access_control_enabled = true
-
-network_profile {
-  network_plugin    = "azure"
-  load_balancer_sku = "standard"
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
   }
 }
+
+resource "azurerm_role_assignment" "aks_acr" {
+  scope              = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id       = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "kubeconfig" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
 
