5be80a5f107d4e113eea165d00d7ddf702911aa0
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-21 14:51:16 +0200

    Update k8s-cluster.tf

--- PracticaFinal/infra/terraform/k8s-cluster.tf
+++ PracticaFinal/infra/terraform/k8s-cluster.tf
@@ -3,8 +3,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = "techwave"
-
-  kubernetes_version = "1.27.3" 
+  kubernetes_version  = "1.27.3"
 
   default_node_pool {
     name       = "default"
@@ -16,9 +15,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 
+  role_based_access_control_enabled = true
 
-   role_based_access_control_enabled = true
-
-  
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
 }
-
