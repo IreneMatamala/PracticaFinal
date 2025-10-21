6a853972643e9af30bc9603bd679fdb4d447f6a8
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-21 15:12:18 +0200

    Update main.tf

--- PracticaFinal/infra/terraform/main.tf
+++ PracticaFinal/infra/terraform/main.tf
@@ -1,59 +1,14 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.100.0"
+      version = ">= 3.77.0"
     }
   }
-
-  required_version = ">= 1.5.0"
-
-  backend "local" {}
 }
 
 provider "azurerm" {
   features {}
 }
-
-
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-
-resource "azurerm_virtual_network" "main" {
-  name                = "techwave-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-}
-
-resource "azurerm_subnet" "main" {
-  name                 = "techwave-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-
-resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  sku                 = "Basic"
-  admin_enabled       = true
-}
-
-
-
-resource "azurerm_role_assignment" "aks_acr" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
-}
-
-
-
-
-
