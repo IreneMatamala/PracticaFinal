750e3a7e9b96bb6c03266b52c9734e016b3f363a
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-21 16:07:12 +0200

    Update main.tf

--- PracticaFinal/infra/terraform/main.tf
+++ PracticaFinal/infra/terraform/main.tf
@@ -12,48 +12,43 @@ provider "azurerm" {
   client_secret   = var.client_secret
 }
 
-
 resource "azurerm_resource_group" "rg" {
-  name     = "techwave-rg"
-  location = "westeurope"
+  name     = var.resource_group_name
+  location = var.location
 }
 
-
 resource "azurerm_virtual_network" "vnet" {
-  name                = "techwave-vnet"
+  name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-
 resource "azurerm_subnet" "subnet" {
-  name                 = "techwave-subnet"
+  name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-
 resource "azurerm_container_registry" "acr" {
-  name                     = "techwaveacr123"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  sku                      = "Basic"
-  admin_enabled            = true
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
 }
 
-
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  name                = "techwave-aks"
+  name                = var.aks_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "techwaveaks"
 
   default_node_pool {
     name       = "default"
-    node_count = 2
-    vm_size    = "Standard_DS2_v2"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
     vnet_subnet_id = azurerm_subnet.subnet.id
   }
 
@@ -62,21 +57,16 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   }
 
   network_profile {
-    network_plugin    = "azure"
-    load_balancer_sku = "standard"
-    outbound_type     = "loadBalancer"
+    network_plugin = "azure"
+    network_policy = "azure"
+    service_cidr   = "10.2.0.0/16"
+    dns_service_ip = "10.2.0.10"
+    docker_bridge_cidr = "172.17.0.1/16"
   }
-
-  depends_on = [
-    azurerm_subnet.subnet
-  ]
 }
-
 
 resource "azurerm_role_assignment" "aks_acr" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
 }
-
-
