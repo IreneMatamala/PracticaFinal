7653ac5a9ce9754a00780ec4472770d476ab4b90
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-23 13:49:20 +0200

    Update provider.tf

--- PracticaFinal/infra/terraform/provider.tf
+++ PracticaFinal/infra/terraform/provider.tf
@@ -1,7 +1,3 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "aa9408e4-354b-46bd-af99-ef533fa2b812"
-}
 
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
