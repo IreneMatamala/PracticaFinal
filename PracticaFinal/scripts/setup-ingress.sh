c9e7f916ddc660209a09144b022fde9019cba3f3
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-23 11:38:11 +0200

    Create setup-ingress.sh

---
+++ PracticaFinal/scripts/setup-ingress.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Installing NGINX Ingress Controller..."
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx \
+  --create-namespace \
+  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz
+
+echo "Waiting for Ingress Controller to be ready..."
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=300s
+
+echo "Getting the external IP..."
+kubectl get service ingress-nginx-controller --namespace ingress-nginx
