61752a6e14a27549696e9d949c8829caa867eb15
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-27 16:17:35 +0100

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -49,9 +49,15 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     dns_service_ip = "10.2.0.10"
     
   }
+
 }
 
-
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+}
 
 
 
